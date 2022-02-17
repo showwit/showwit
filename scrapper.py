f3cf085a60104ad3b662afe3c738865a03c74f32
Aryan Agarwal <aryan23062001@gmail.com>
2022-02-17 17:32:34 +0530

    'first'

---
+++ scrapper.py
@@ -0,0 +1,20 @@
+from cgitb import html
+import requests
+from bs4 import BeautifulSoup
+import flask
+from flask import request
+
+URL = 'http://mathanosto.top/movie.php?imdbid=' + 'tt10733228' + '&cat=' + 'Bollywood'
+html_text = requests.get(URL).text
+soup = BeautifulSoup(html_text, 'lxml')
+moviePlayURL  = soup.find('a', class_='btn-element btn btn-fullcolor btn-block btn-fullwidth')['href']
+print(moviePlayURL)
+
+
+app = flask.Flask(__name__)
+
+@app.route('/' , methods=['GET'])
+def home():
+    arg  = request.args['arg1']
+    return moviePlayURL
+
