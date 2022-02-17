94b152446d136e2f169b946258e2508a535418d8
Aryan Agarwal <aryan23062001@gmail.com>
2022-02-17 18:07:42 +0530

    commited

--- scrapper.py
+++ scrapper.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 import flask
 from flask import request
 
-URL = 'http://mathanosto.top/movie.php?imdbid=' + 'tt10733228' + '&cat=' + 'Bollywood'
-html_text = requests.get(URL).text
-soup = BeautifulSoup(html_text, 'lxml')
-moviePlayURL  = soup.find('a', class_='btn-element btn btn-fullcolor btn-block btn-fullwidth')['href']
+# URL = 'http://mathanosto.top/movie.php?imdbid=' + 'tt10733228' + '&cat=' + 'Bollywood'
+# html_text = requests.get(URL).text
+# soup = BeautifulSoup(html_text, 'lxml')
+# moviePlayURL  = soup.find('a', class_='btn-element btn btn-fullcolor btn-block btn-fullwidth')['href']
 
 
 app = flask.Flask(__name__)
